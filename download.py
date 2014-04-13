dc250ab79194aaeba92b7010bf6a84ad3d0ee23c
Charles Lewis <noodle@example.com>
2014-04-13 15:06:33 -0400

    NEW LINESgit add download.py

--- download.py
+++ download.py
@@ -2,4 +2,4 @@ import os,bmemcached
 mc = bmemcached.Client(os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
         os.environ.get('MEMCACHEDCLOUD_USERNAME'),
         os.environ.get('MEMCACHEDCLOUD_PASSWORD'))
-print repr(mc.get('stale_scores'))
+print repr(mc.get('stale_scores')).replace("],","],\n")
